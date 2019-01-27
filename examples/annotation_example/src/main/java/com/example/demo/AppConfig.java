b281b15bef0133807dd3ba4e2693404298d789e8
mithunkumarc <mithunchannappa@gmail.com>
2019-01-27 11:33:46 +0530

    Add files via upload

---
+++ examples/annotation_example/src/main/java/com/example/demo/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.example.demo")
+public class AppConfig {
+   @Bean
+   public Employee getEmployee() {
+      return new Manager();
+   }
+}
---
+++ examples/annotation_example/src/main/java/com/example/demo/Manager.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+
+public class Manager implements Employee {
+
+   @Override
+   public void showEmployeeInfo() {
+      System.out.println("Inside Manager showEmployeeInfo() method.");
+   }
+
+}
