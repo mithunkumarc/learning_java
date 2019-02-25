4d804dc75ff7c05a798a049a3c9dab1c95b15629
mithunkumarc <mithunchannappa@gmail.com>
2019-02-25 11:13:52 +0530

    Add files via upload

---
+++ examples/19 mvc-crud-h2/src/main/java/com/example/demo/MvcCrudApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+public class MvcCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcCrudApplication.class, args);
+	}
+
+}
+
+
+
+
+
