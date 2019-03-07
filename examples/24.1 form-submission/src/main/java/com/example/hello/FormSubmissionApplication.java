f3d65d9d7cc55dcaf81dbe8c3a7d48a7dd49029a
mithunkumarc <mithunchannappa@gmail.com>
2019-03-07 11:06:35 +0530

    Add files via upload

---
+++ examples/24.1 form-submission/src/main/java/com/example/hello/FormSubmissionApplication.java
@@ -0,0 +1,14 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSubmissionApplication {
+
+	//http://localhost:8080/greeting
+	public static void main(String[] args) {
+		SpringApplication.run(FormSubmissionApplication.class, args);
+	}
+
+}
