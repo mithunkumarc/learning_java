9f729746cd1052ba1fb0d41b8e5d5d8978237385
mithunkumarc <mithunchannappa@gmail.com>
2019-04-04 14:53:48 +0530

    Add files via upload

---
+++ examples/28 jms-queue/src/main/java/com/example/demo/receiver/Receiver.java
@@ -0,0 +1,18 @@
+package com.example.demo.receiver;
+
+
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Email;
+
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        System.out.println("Received <" + email + ">");
+    }
+
+}
