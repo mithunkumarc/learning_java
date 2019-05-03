7ac5a3a0996e90db76c4f3005b2e10a9ee297fad
mithunkumarc <mithunchannappa@gmail.com>
2019-05-03 12:06:36 +0530

    Add files via upload

---
+++ examples/17.1 rest-client/src/main/java/com/example/demo/RestClientApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.model.Quote;
+
+@SpringBootApplication
+public class RestClientApplication {
+
+	
+	public static void main(String args[]) {
+		SpringApplication.run(RestClientApplication.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			System.out.println(quote.toString());
+		};
+	}
+}
