a59493214a9cd38d0498caa5a905b4c7e5232e3b
mithunkumarc <mithunchannappa@gmail.com>
2019-06-22 10:42:43 +0530

    Add files via upload

---
+++ examples/32 rest-basic-auth/src/main/java/com/example/demo/repository/EmployeeDao.java
@@ -0,0 +1,29 @@
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Employee;
+import com.example.demo.model.Employees;
+
+
+@Repository
+public class EmployeeDao 
+{
+    private static Employees list = new Employees();
+    
+    static 
+    {
+        list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+    }
+    
+    public Employees getAllEmployees() 
+    {
+        return list;
+    }
+    
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
