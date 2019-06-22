8f0541645f71f0da145ee0beeae308d10b3d5b6b
mithunkumarc <mithunchannappa@gmail.com>
2019-06-22 11:07:20 +0530

    Update EmployeeController.java

--- examples/32 rest-basic-auth/src/main/java/com/example/demo/controller/EmployeeController.java
+++ examples/32 rest-basic-auth/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Employees;
 import com.example.demo.repository.EmployeeDao;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping(path = "/employees")
@@ -13,9 +15,17 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeDao employeeDao;
 	
-	
+		//url : http://localhost:8085/employees/
 	@GetMapping(path="/",produces = "application/json")
 	public Employees getEmployees() {
 		return employeeDao.getAllEmployees();
+	}
+	
+	
+	
+	//url : http://localhost:8085/employees/2
+	@GetMapping(path="/{id}",produces = "application/json")
+	public Employee getEmployees(@PathVariable("id")int id) {
+		return employeeDao.getEmployee(id);
 	}
 }
