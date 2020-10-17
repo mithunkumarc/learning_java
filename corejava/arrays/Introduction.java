adb561af9b59048c3c7f3bd88cfdeebfad8cf96a
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-10-17 20:41:46 +0530

    Create Introduction.java

---
+++ corejava/arrays/Introduction.java
@@ -0,0 +1,30 @@
+package basics;
+
+/*
+  fixed datastructure
+  can be created for primitive types, build types and user defined types
+*/
+
+class Customer{
+	
+}
+
+public class Introduction {
+
+	public static void main(String[] args) {
+  
+    // for primitive types
+	  int[] numbers = new int[5];
+	  char[] letters = {'a','b','c','d'};
+    
+    // array of builtin type
+	  String[] emptyArry = {};
+    		
+    // array of user defined type
+    Customer[] customers = new Customer[4]; // array with user defined type
+	
+    // two dimensional array
+    float[][] data = {{1.1f, 2.2f, 3.3f},{4.4f, 5.5f, 6.6f}};
+
+  }	
+}
